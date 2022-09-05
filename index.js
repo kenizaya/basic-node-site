19b11ed4c7d87b485cebf573aad713b5f65d68f7
kenizaya <harveylstark@gmail.com>
2022-09-05 15:33:43 +0530

    Create basic server

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.end('<h1>Home</>')
+  }
+})
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+  console.log('Server running at port', PORT)
+})
